806f952c2551aee2ba57aaedeaf4f550f6be7342
Michael Walker <michaelwwalker42@gmail.com>
2022-01-07 18:37:34 -0700

    initial files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,16 @@
+// The first class is an Employee parent class 
+// with the following properties and methods:
+
+// name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole() // Returns 'Employee'
