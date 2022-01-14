66e19980f2cf57909793b3ff6b75198eaf11f707
Michael Walker <michaelwwalker42@gmail.com>
2022-01-13 18:54:45 -0700

    added generateHtml.js to src folder

---
+++ src/generateHtml.js
@@ -0,0 +1,33 @@
+
+
+function generateHtml(data) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/9481a02745.js" crossorigin="anonymous"></script>
+    <title>Team Profile</title>
+</head>
+
+<body>
+
+    <div class="col-12 text-center text-white py-1 bg-danger">
+        <h1>Team Name</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="d-flex flex-wrap justify-content-center align-items-center">
+            `
+    
+};
+
+module.exports = generateHtml;
